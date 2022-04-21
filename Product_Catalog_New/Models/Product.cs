04a6201c3073c85440ca6cafd53b6779c9b500c9
Lukas Hödl <lukas.hoedl@outlook.com>
2022-04-21 21:21:16 +0200

    Product Model

---
+++ Product_Catalog_New/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Product_Catalog_New.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public float Price { get; set; }
+
+        public string? Description { get; set; }
+
+
+
+
+    }
+}
